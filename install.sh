88e14ccdd469e84c869541f509548e5fd964c705
root <root@vps.server.local>
2022-04-28 15:07:24 +0000

    init1

---
+++ install.sh
@@ -0,0 +1,6 @@
+curl -sL https://deb.nodesource.com/setup_current.x | sudo bash -
+sudo apt install nodejs
+npm install nodemon -g
+npm install http-server -g
+cd ./wrapper
+npm install
--- sw.sh
+++ sw.sh
@@ -1,9 +1,4 @@
-# nodemon --exec "http-server -p 4664 -c-1 -S -C the.crt -K the.key"
-# nodemon server.js
-curl -sL https://deb.nodesource.com/setup_current.x | sudo bash -
-sudo apt install nodejs
-npm install nodemon
-cd server
-nodemon --exec "http-server -a 0.0.0.0 -p 4664 -c-1 -S -C the.crt -K the.key" ./
-cd ../wrapper
+cd ./wrapper
 nodemon --exec "node server.js" ./ &
+cd ../server
+nodemon --exec "http-server -a 0.0.0.0 -p 4664 -c-1 -S -C the.crt -K the.key" ./ &
